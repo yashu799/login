f3ff3f50240097b57a9b761bf973837c43762e94
yashu799 <yash.patil16431@sakec.ac.in>
2024-03-07 16:18:30 +0530

    Add files via upload

---
+++ forgot-password.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forgot password</title>
+    <link rel="stylesheet" href="https://unpkg.com/mvp.css">
+
+</head>
+<body>
+    <h2>Forgot password</h2>
+    <form method="post" action="send-password-reset.php">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email">
+
+        <button>Send</button>
+    </form>
+    
+</body>
+</html>
